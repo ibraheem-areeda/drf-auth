19cf0fb46224cd5b20c3d675f435bc2ca4ce55fd
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-08-02 12:04:32 +0000

    befor start app

--- auth_items_api/settings.py
+++ auth_items_api/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # 'rest_framework_simplejwt',
     # local apps
     'accounts',
-    # 'things'
+    # 'items'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
